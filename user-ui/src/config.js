d1f51c79d579153320b1c4570934d7ffa2e5a23a
Naveen SILVER <120022254+naveensilver@users.noreply.github.com>
2024-11-30 18:39:00 +0530

    Update config.js

--- user-ui/src/config.js
+++ user-ui/src/config.js
@@ -1,3 +1,3 @@
-export const CONTACT_EMAIL = 'deekshithsn@gmail.com'
-export const PAYMENT_NUMBER = '99xxx33xxx'
+export const CONTACT_EMAIL = 'thurkapallynaveen00@gmail.com'
+export const PAYMENT_NUMBER = '95xxx19xxx'
 
